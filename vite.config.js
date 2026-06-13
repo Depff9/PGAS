e7af3ff2177957e82b132b3c9f3adb531556ae7b
Depff9 <goshabryakov9@gmail.com>
2026-06-14 06:29:44 +0800

    fast update

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,24 @@ export default defineConfig({
   server: {
     port: 5173,
     open: true,
+    host: true,
+    allowedHosts: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:4000',
+        changeOrigin: true,
+      },
+    },
+  },
+  preview: {
+    host: '127.0.0.1',
+    port: 4173,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:4000',
+        changeOrigin: true,
+      },
+    },
   },
 });
